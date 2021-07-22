fe388cdc5c38585e9b15886bc375a8df241e9eea
Cimpean Rares <rares_cimpean@yahoo.com>
2021-07-22 18:48:53 +0300

    Adaugat eroare 404 pentru id mai mare decat numarul de inregistrari sau nul

--- view.php
+++ view.php
@@ -6,15 +6,21 @@
 if (isset($_GET['id'])) {
     $postID = $_GET['id'];
     $dbconn = pg_connect("host=localhost port=5432 dbname=blog user=postgres");
-    $result = pg_query_params($dbconn, 'SELECT title,body FROM posts WHERE id=$1', array($postID));
-
-    echo '<table border="3" style="width: 100%;">';
-    while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-        echo "\t<tr>\n";
-        foreach ($line as $col_value) {
-            echo "\t\t<td>$col_value</td>\n";
+    $total_pages_sql = pg_query($dbconn, "SELECT COUNT (*) FROM posts");
+    $total_rows = pg_fetch_array($total_pages_sql)[0];
+    if ($postID>$total_rows||$postID<=0) {
+        header("HTTP/1.0 404 Not Found");
+        echo 'Postarea nu a fost gasita';
+    } else {
+        $result = pg_query_params($dbconn, 'SELECT title,body FROM posts WHERE id=$1', array($postID));
+        echo '<table border="3" style="width: 100%;">';
+        while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+            echo "\t<tr>\n";
+            foreach ($line as $col_value) {
+                echo "\t\t<td>$col_value</td>\n";
+            }
+            echo "\t</tr>\n";
         }
-        echo "\t</tr>\n";
     }
 } else {
     header('Location: http://127.0.0.1:9000/index.php?pageno=1');
